cd42af93074bc2de04d629ec8f71e36445bf9dde
Nikonenko Daniil <alfa_fk@mail.ru>
2020-05-17 13:55:02 +0300

    comment

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "std.h"
+
+int main(){
+  int A , B ,exit;
+  std::cout<<"write A: ";std::cin>>A;
+  std::cout<<"write B: ";std::cin>>B;
+  std::cout<<"sum is equal:"<<A+B<<std::endl;
+  std::cin>>exit;
+}
