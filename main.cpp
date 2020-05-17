aea612f1ae3807211da8f6c4c3acf00e5d93f696
Nikonenko Daniil <alfa_fk@mail.ru>
2020-05-17 13:58:27 +0300

    main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main(){
   int A , B ,exit;
   std::cout<<"write A: ";std::cin>>A;
   std::cout<<"write B: ";std::cin>>B;
-  std::cout<<"sum:"<<A+B<<std::endl;
+  std::cout<<"sum is :"<<A+B<<std::endl;
   std::cin>>exit;
 }
