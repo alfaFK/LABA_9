9c30028dbf20ec32677217c5f16c7fc4ad4bbb3c
alfaFK <43901388+alfaFK@users.noreply.github.com>
2020-05-18 11:13:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main(){
   std::cout<<"write A: ";std::cin>>A;
   std::cout<<"write B: ";std::cin>>B;
   std::cout<<"sum is :"<<A+B<<std::endl;
+  std::cout<<"sum is equal:"<<A+B<<std::endl;
+  std::cout<<"Subtraction is equal:"<<A-B<<std::endl;
   std::cin>>exit;
 }
