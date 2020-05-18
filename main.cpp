f730b95ec7077e37bd7abab9c1552b8a4ca5aff3
alfaFK <43901388+alfaFK@users.noreply.github.com>
2020-05-18 11:26:43 +0300

    Update main.cpp
    add subtraction

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main(){
   int A , B ,exit;
-  std::cout<<"write A: ";std::cin>>A;
-  std::cout<<"write B: ";std::cin>>B;
+  std::cout<<"write A : ";std::cin>>A;
+  std::cout<<"write B : ";std::cin>>B;
   std::cout<<"sum is :"<<A+B<<std::endl;
   std::cout<<"sum is equal:"<<A+B<<std::endl;
   std::cout<<"Subtraction is equal:"<<A-B<<std::endl;
