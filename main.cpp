c3bd125239df0b4f9d6224ac03d8775c434c72b5
Nikonenko Daniil <alfa_fk@mail.ru>
2020-05-19 11:18:11 +0300

    division

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(){
   std::cout<<"write B: ";std::cin>>B;
   std::cout<<"sum is :"<<A+B<<std::endl;
   std::cout<<"Subtract is :"<<A-B<<std::endl;
-  std::cout<<"Multiplication is :"<<A*B<<std::endl;
+  std::cout<<"Division is :"<<A/B<<std::endl;
   std::cin>>exit;
 }
