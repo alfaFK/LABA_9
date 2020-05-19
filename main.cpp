316297d22b360e73386154d855ea8db8e05e5967
Nikonenko Daniil <alfa_fk@mail.ru>
2020-05-19 11:11:20 +0300

    «multiplication»

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "std.h"
-
+//This program is an educational
 int main(){
   int A , B ,exit;
   std::cout<<"write A: ";std::cin>>A;
   std::cout<<"write B: ";std::cin>>B;
   std::cout<<"sum is :"<<A+B<<std::endl;
+  std::cout<<"Subtract is :"<<A-B<<std::endl;
+  std::cout<<"Multiplication is :"<<A*B<<std::endl;
   std::cin>>exit;
 }
